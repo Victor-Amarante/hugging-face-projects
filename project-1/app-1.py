bea8c8eb0bc16e513166f8508e62a986ee7750ae
Victor Amarante <vafstats@gmail.com>
2024-07-24 14:07:20 -0300

    refactor: rename variable 'phrase' to 'sentence' in app-1.py and some texts in README

--- project-1/app-1.py
+++ project-1/app-1.py
@@ -3,15 +3,15 @@ from transformers import pipeline
 
 base_model = pipeline('fill-mask', model='distilroberta-base')
 
-phrase = 'The capital of <mask> is Brasilia.'
+sentence = 'The capital of <mask> is Brasilia.'
 
-predictions = base_model(phrase)
+predictions = base_model(sentence)
 
 for predict in predictions:
     score = predict['score']
     score_adj = score * 100
     response_mask = predict['token_str']
-    phrase_markered = predict['sequence']
+    sentence_markered = predict['sequence']
     
-    print(f'Predict "{response_mask.strip()}" with score {score_adj:.2f}% -> {phrase_markered}')
+    print(f'Predict "{response_mask.strip()}" with score {score_adj:.2f}% -> {sentence_markered}')
     
